06d1e87875a99d72a2e86272c96005eb4e2e3124
kimbank <kimeunhang@inha.edu>
2023-12-22 04:54:48 +0900

    fix: root layout 변경 및 세션 모니터 툴 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,43 @@
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Hotjar from "@/utils/hotjar";
+import DatadogRum from "@/utils/datadogRum";
 
 import { theme } from "@/assets";
 import ReduxProvider from "@/store/provider.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Header } from "./components/Header/Header";
+
+export const metadata = {
+  title: '아무나 만나지 마세요, 연애정보회사 ONLYou',
+  description: '꼭 맞는 사람만 신중하게 매칭하는 소개팅 서비스에요. 외모, 가치관, 연애스타일 등 42개의 항목과 그 중요도까지 설정할 수 있어요.',
+}
 
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) => {
   return (
     <html lang="ko-KR" translate="no">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <Hotjar />
+        <DatadogRum />
+      </head>
       <body>
         <ThemeProvider theme={theme}>
           <ReduxProvider>
             <div id="root">
               <div className="page">
-                <Header />
+                {/* <Header /> */}
                 <CssBaseline>
                   {children}
                 </CssBaseline>
               </div>
             </div>
-            {/* { process.env.NEXT_PUBLIC_HOTJAR === 'true' && <Hotjar />} */}
           </ReduxProvider>
         </ThemeProvider>
         <SpeedInsights />
@@ -33,3 +45,6 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+export default RootLayout;
---
+++ src/utils/datadogRum.js
@@ -0,0 +1,36 @@
+const datadogRum = () => {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,u,n,d) {
+            h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
+            d=o.createElement(u);d.async=1;d.src=n
+            n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
+          })(window,document,'script','https://www.datadoghq-browser-agent.com/us5/v5/datadog-rum.js','DD_RUM')
+          window.DD_RUM.onReady(function() {
+            window.DD_RUM.init({
+              clientToken: 'pub66262f6057584eeedf737f7b5839d12b',
+              applicationId: '0bc3679b-06d7-48a3-b210-1110ac9abcbb',
+              site: 'us5.datadoghq.com',
+              service: 'onlyou-v2-front',
+              env: 'prod',
+              // Specify a version number to identify the deployed version of your application in Datadog 
+              version: '2.0.0', 
+              sessionSampleRate: 100,
+              sessionReplaySampleRate: 20,
+              trackUserInteractions: true,
+              trackResources: true,
+              trackLongTasks: true,
+              defaultPrivacyLevel: 'mask-user-input',
+            });
+          })
+            `,
+          }} 
+      />
+    </>
+  )
+}
+
+export default datadogRum;
